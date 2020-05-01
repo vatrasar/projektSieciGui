e956720c0c33f20c09230948ba3ea35da0b749db
vatrasar <vatrasar@gmail.com>
2020-05-01 17:58:46 +0200

    Debug file can contains more then 3 sensors

--- lab4/Main.java
+++ lab4/Main.java
@@ -127,20 +127,24 @@ public class Main {
 			printWriter.println("#parameters of simulation: WSN-2d,POI-36");
 			printWriter.println("# who is ON/OFF");
 			printWriter.println("#s_num s_i s_i-1 s_i-2 q q_curr rev");
+
 			allSensors.forEach(sensor->sensor.setStan(0));
 			list.forEach(sensor->sensor.setStan(1));
-
-			printWriter.println(String.format("%d %d %d %d %.2f %.2f %.2f",1,allSensors.get(0).getStan(),allSensors.get(1).getStan(),allSensors.get(2).getStan(),data.getQ(),allSensors.get(0).getCurrentLocalCoverageRate(),allSensors.get(0).computeReword(data)));
-
-
-			printWriter.println(String.format("%d %d %d %d %.2f %.2f %.2f",2,allSensors.get(1).getStan(),allSensors.get(0).getStan(),allSensors.get(2).getStan(),data.getQ(),allSensors.get(1).getCurrentLocalCoverageRate(),allSensors.get(1).computeReword(data)));
-
-			printWriter.println(String.format("%d %d %d %d %.2f %.2f %.2f",3,allSensors.get(2).getStan(),allSensors.get(0).getStan(),allSensors.get(1).getStan(),data.getQ(),allSensors.get(2).getCurrentLocalCoverageRate(),allSensors.get(2).computeReword(data)));
+			String outString=DebugFile.generate(data);
+			printWriter.print(outString);
+//			printWriter.println(String.format("%d %d %d %d %.2f %.2f %.2f",1,allSensors.get(0).getStan(),allSensors.get(1).getStan(),allSensors.get(2).getStan(),data.getQ(),allSensors.get(0).getCurrentLocalCoverageRate(),allSensors.get(0).computeReword(data)));
+//
+//
+//			printWriter.println(String.format("%d %d %d %d %.2f %.2f %.2f",2,allSensors.get(1).getStan(),allSensors.get(0).getStan(),allSensors.get(2).getStan(),data.getQ(),allSensors.get(1).getCurrentLocalCoverageRate(),allSensors.get(1).computeReword(data)));
+//
+//			printWriter.println(String.format("%d %d %d %d %.2f %.2f %.2f",3,allSensors.get(2).getStan(),allSensors.get(0).getStan(),allSensors.get(1).getStan(),data.getQ(),allSensors.get(2).getCurrentLocalCoverageRate(),allSensors.get(2).computeReword(data)));
 			printWriter.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
+
+
 
 	private static void saveExperimentDataToFile(Dane data) {
 
